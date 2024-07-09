fc3ca0bd94a642b061a61c9704257732821f31de
timothyas <smith.timothy.andrew@gmail.com>
2024-07-09 16:38:25 +0000

    update evaluation scripts

--- evaluate-graphcast.sh
+++ evaluate-graphcast.sh
@@ -2,19 +2,20 @@
 
 export PYTHONPATH=$PYTHONPATH:$PWD/weatherbench2:$PWD/weatherbench2/weatherbench2:$PWD/weatherbench2/scripts
 
+year=2018
+
 python weatherbench2/scripts/evaluate.py \
- --forecast_path=gs://weatherbench2/datasets/graphcast/2020/date_range_2019-11-16_2021-02-01_12_hours-64x32_equiangular_conservative.zarr \
- --obs_path=gs://weatherbench2/datasets/era5/1959-2022-6h-64x32_equiangular_conservative.zarr \
- --climatology_path=gs://weatherbench2/datasets/era5-hourly-climatology/1990-2019_6h_64x32_equiangular_conservative.zarr \
- --output_dir=./ \
- --output_file_prefix=graphcast_vs_era_2020_ \
+ --forecast_path=gs://weatherbench2/datasets/graphcast/${year}/date_range_2017-11-16_2019-02-01_12_hours-240x121_equiangular_with_poles_conservative.zarr \
+ --obs_path=gs://weatherbench2/datasets/era5/1959-2023_01_10-6h-240x121_equiangular_with_poles_conservative.zarr \
+ --climatology_path=gs://weatherbench2/datasets/era5-hourly-climatology/1990-2019_6h_240x121_equiangular_with_poles_conservative.zarr \
+ --output_dir=./prototypes/p1/results/v1/validation \
+ --output_file_prefix=graphcast_vs_era_${year}_ \
+ --by_init=True \
  --input_chunks=init_time=1 \
- --fanout=27 \
- --regions=all \
- --eval_configs=deterministic,deterministic_temporal \
+ --eval_configs=deterministic \
  --evaluate_climatology=False \
  --evaluate_persistence=False \
- --time_start=2020-01-01 \
- --time_stop=2020-12-31 \
- --variables=geopotential,temperature,u_component_of_wind,v_component_of_wind,specific_humidity,2m_temperature,10m_u_component_of_wind,10m_v_component_of_wind,mean_sea_level_pressure
- 
+ --time_start=${year}-01-01T06 \
+ --time_stop=${year}-01-03T18 \
+ --variables="2m_temperature,10m_u_component_of_wind,10m_v_component_of_wind,mean_sea_level_pressure" \
+ --levels=100,500,850
--- evaluate-graphufs.sh
+++ evaluate-graphufs.sh
@@ -2,31 +2,26 @@
 
 export PYTHONPATH=$PYTHONPATH:$PWD/weatherbench2:$PWD/weatherbench2/weatherbench2:$PWD/weatherbench2/scripts
 
+forecast_duration="24h"
 
-python weatherbench2/scripts/evaluate.py \
-  --forecast_path=prototypes/p1/results/v1/validation/predictions.fakeplevel.24h.zarr \
-  --obs_path=gs://weatherbench2/datasets/era5/1959-2023_01_10-6h-240x121_equiangular_with_poles_conservative.zarr \
-  --climatology_path=gs://weatherbench2/datasets/era5-hourly-climatology/1990-2019_6h_240x121_equiangular_with_poles_conservative.zarr \
-  --by_init=False \
-  --output_dir=./prototypes/p1/results/v1/validation \
-  --output_file_prefix=predictions_vs_era5_fakeplevel_24h_ \
-  --input_chunks=init_time=1 \
-  --eval_configs=deterministic,deterministic_temporal \
-  --time_start=2022-01-01T06 \
-  --time_stop=2022-01-02T18 \
-  --variables="surface_pressure,10m_u_component_of_wind,10m_v_component_of_wind,2m_temperature,temperature,u_component_of_wind,v_component_of_wind,vertical_velocity,specific_humidity" \
-  --levels=100,500,850
+for dataset in "predictions" "replay"
+do
+    echo "Evaluating ${dataset} ..."
+    python weatherbench2/scripts/evaluate.py \
+      --forecast_path=prototypes/p1/results/v1/validation/${dataset}.fakeplevel.${forecast_duration}.zarr \
+      --obs_path=gs://weatherbench2/datasets/era5/1959-2023_01_10-6h-240x121_equiangular_with_poles_conservative.zarr \
+      --climatology_path=gs://weatherbench2/datasets/era5-hourly-climatology/1990-2019_6h_240x121_equiangular_with_poles_conservative.zarr \
+      --by_init=True \
+      --output_dir=./prototypes/p1/results/v1/validation \
+      --output_file_prefix=${dataset}_vs_era5_fakeplevel_${forecast_duration}_ \
+      --eval_configs=deterministic,deterministic_temporal \
+      --time_start=2022-01-01T06 \
+      --time_stop=2022-01-03T18 \
+      --evaluate_climatology=False \
+      --evaluate_persistence=False \
+      --variables="surface_pressure,10m_u_component_of_wind,10m_v_component_of_wind,2m_temperature" \
+      --levels=100,500,850
+    echo " ... done with ${dataset}"
+done
 
-python weatherbench2/scripts/evaluate.py \
-  --forecast_path=prototypes/p1/results/v1/validation/replay.fakeplevel.24h.zarr \
-  --obs_path=gs://weatherbench2/datasets/era5/1959-2023_01_10-6h-240x121_equiangular_with_poles_conservative.zarr \
-  --climatology_path=gs://weatherbench2/datasets/era5-hourly-climatology/1990-2019_6h_240x121_equiangular_with_poles_conservative.zarr \
-  --by_init=False \
-  --output_dir=./prototypes/p1/results/v1/validation \
-  --output_file_prefix=replay_vs_era5_fakeplevel_24h_ \
-  --input_chunks=init_time=1 \
-  --eval_configs=deterministic,deterministic_temporal \
-  --time_start=2022-01-01T06 \
-  --time_stop=2022-01-02T18 \
-  --variables="surface_pressure,10m_u_component_of_wind,10m_v_component_of_wind,2m_temperature,temperature,u_component_of_wind,v_component_of_wind,vertical_velocity,specific_humidity" \
-  --levels=100,500,850
+
