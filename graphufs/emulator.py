24ed01b74c276dbfd6469242f01bd6cadc1b6cfa
timothyas <tim.smith@noaa.gov>
2024-10-29 13:38:11 -0600

    gradient clipping test... inconclusive

--- graphufs/emulator.py
+++ graphufs/emulator.py
@@ -71,6 +71,7 @@ class ReplayEmulator:
 
     # training protocol
     batch_size = None               # number of forecasts averaged over in loss per optim_step
+    grad_clip_value = 32.
     num_batch_splits = None         # number of batch splits
     num_epochs = None               # number of epochs
     chunks_per_epoch = None         # number of chunks per epoch
@@ -963,6 +964,8 @@ class ReplayEmulator:
 
         with open(ckpt_path, "rb") as f:
             ckpt = checkpoint.load(f, CheckPoint)
+
+        print("Loaded checkpoint from ", f)
         params = ckpt.params
         state = {}
         model_config = ckpt.model_config
