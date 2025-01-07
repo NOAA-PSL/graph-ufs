47cf261570b167f0e14206f3a3e48e6b2ddf2a38
Niraj Agarwal <niraj.agarwal@noaa.gov>
2025-01-07 09:33:13 -0800

    removed duplicate module import

--- graphufs/__init__.py
+++ graphufs/__init__.py
@@ -2,5 +2,4 @@ from .emulator import ReplayEmulator
 from .coupledemulator import ReplayCoupledEmulator
 from .fvemulator import FVEmulator
 from .fvcoupledemulator import FVCoupledEmulator
-from .coupledemulator import ReplayCoupledEmulator
 from .statistics import StatisticsComputer, add_derived_vars
