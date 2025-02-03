2e80049d62afb9e3e9a584d9ae2c2edb89fc39a6
Niraj Agarwal <niraj.agarwal@noaa.gov>
2025-02-03 16:20:30 -0700

    stacked CP0 prototype: same as CP0 but using the stacked framework

---
+++ prototypes/stacked_cp0/config.py
@@ -0,0 +1,228 @@
+import os
+import xarray as xr
+from jax import tree_util
+import numpy as np
+
+from graphufs import FVCoupledEmulator
+
+
+stackedcp0_path = os.path.dirname(os.path.realpath(__file__))
+
+def log(xda):
+    cond = xda > 0
+    return xr.where(
+        cond,
+        np.log(xda.where(cond)),
+        0.,
+    )
+
+def exp(xda):
+    return np.exp(xda)
+
+class StackedCP0Emulator(FVCoupledEmulator):
+    data_url = {}
+    norm_urls = {}
+
+    norm_url = {
+        "mean": f"{stackedcp0_path}/fvstatistics.1994/mean_by_level.zarr",
+        "std": f"{stackedcp0_path}/fvstatistics.1994/stddev_by_level.zarr",
+        "stddiff": f"{stackedcp0_path}/fvstatistics.1994/diffs_stddev_by_level.zarr",
+    }
+    data_url["atm"] = "gcs://noaa-ufs-gefsv13replay/ufs-hr1/0.25-degree-subsampled/03h-freq/zarr/fv3.zarr"
+    norm_urls["atm"] = norm_url
+
+    data_url["ocn"] = "gcs://noaa-ufs-gefsv13replay/ufs-hr1/0.25-degree-subsampled/06h-freq/zarr/mom6.zarr"
+    norm_urls["ocn"] = norm_url
+
+    data_url["ice"] = "gcs://noaa-ufs-gefsv13replay/ufs-hr1/0.25-degree-subsampled/03h-freq/zarr/fv3.zarr"
+    norm_urls["ice"] = norm_url
+    
+    data_url["land"] = "gcs://noaa-ufs-gefsv13replay/ufs-hr1/0.25-degree-subsampled/03h-freq/zarr/fv3.zarr"
+    norm_urls["land"] = norm_url
+
+    wb2_obs_url = "gs://weatherbench2/datasets/era5/1959-2022-6h-64x32_equiangular_conservative.zarr"
+
+    local_store_path = f"{stackedcp0_path}/local-output"
+    cache_data = True
+
+    # these could be moved to a yaml file later
+    # task config options
+    # inputs
+    atm_input_variables = (
+        # Surface Variables
+        "pressfc",
+        #"ugrd10m",
+        #"vgrd10m",
+        #"tmp2m",
+        "spfh2m",
+        # 3D Variables
+        "tmp",
+        "spfh",
+        #"ugrd",
+        #"vgrd",
+        #"dzdt",
+        # Forcing Variables at input time
+        "dswrf_avetoa",
+        "year_progress_sin",
+        "year_progress_cos",
+        "day_progress_sin",
+        "day_progress_cos",
+    )
+    ocn_input_variables = (
+        # Surface Variables
+        "SSH",
+        #"LW",
+        #"SW",
+        # 3D Variables
+        "so",
+        "temp",
+        #"uo",
+        #"vo",
+        #"landsea_mask",
+    )
+    ice_input_variables = (
+        "icec",
+        #"icetk",
+    )
+    land_input_variables = (
+        "soilm",
+        #"soilt1",
+        #"tmpsfc",
+    )
+
+    # targets
+    atm_target_variables = (
+        # Surface Variables
+        "pressfc",
+        #"ugrd10m",
+        #"vgrd10m",
+        #"tmp2m",
+        "spfh2m",
+        # 3D Variables
+        "tmp",
+        "spfh",
+        #"ugrd",
+        #"vgrd",
+        #"dzdt",
+    )
+    ocn_target_variables = (
+        # Surface Variables
+        "SSH",
+        #"LW",
+        #"SW",
+        # 3D Variables
+        "so",
+        "temp",
+        #"uo",
+        #"vo",
+    )
+    ice_target_variables = (
+        "icec",
+        #"icetk",
+    )
+    land_target_variables = (
+        "soilm",
+        #"soilt1",
+        #"tmpsfc",
+    )
+
+    # forcing
+    atm_forcing_variables = (
+        "dswrf_avetoa",
+        "year_progress_sin",
+        "year_progress_cos",
+        "day_progress_sin",
+        "day_progress_cos",
+    )
+    ocn_forcing_variables = ()
+    ice_forcing_variables = ()
+    land_forcing_variables = ()
+
+    all_variables = tuple() # this is created in __init__
+    interfaces = {}
+    interfaces["atm"] = (400, 600, 800, 1000)
+    interfaces["ocn"] = (0, 50, 200, 300, 500)
+    interfaces["ice"] = ()
+    interfaces["land"] = ()
+
+    # time related
+    delta_t = "6h"              # the model time step
+    input_duration = "12h"      # time covered by initial condition(s) + delta_t (necessary for GraphCast code)
+    target_lead_time = "6h"     # how long is the forecast ... at what point do we compare model to targets
+    training_dates = (          # bounds of training data (inclusive)
+        "1994-01-01T00",        # start
+        "1994-03-01T00"         # stop
+    )
+    testing_dates = (           # bounds of testing data (inclusive)
+        "1995-01-01T00",        # start
+        "1995-01-31T18"         # stop
+    )
+    validation_dates = (        # bounds of validation data (inclusive)
+        "1996-01-01T00",        # start
+        "1996-01-31T21"         # stop
+    )
+
+    # training protocol
+    batch_size = 16
+    num_batch_splits = 1
+    num_epochs = 10
+
+    # model config options
+    resolution = 1.0
+    mesh_size = 2
+    latent_size = 256
+    gnn_msg_steps = 4
+    hidden_layers = 1
+    radius_query_fraction_edge_length = 0.6
+
+    # loss weighting, defaults to GraphCast implementation
+    #weight_loss_per_channel = True
+    weight_loss_per_latitude = True
+    weight_loss_per_level = False
+    loss_weights_per_variable = dict()
+    input_transforms = {
+        "spfh": log,
+        "spfh2m": log,
+    }
+    output_transforms = {
+        "spfh": exp,
+        "spfh2m": exp,
+    }
+
+    # this is used for initializing the state in the gradient computation
+    grad_rng_seed = 0
+    init_rng_seed = 0
+    training_batch_rng_seed = 100
+
+    # data chunking options
+    chunks_per_epoch = 1
+    steps_per_chunk = None
+    checkpoint_chunks = 1
+    max_queue_size = 1
+    num_workers = 1
+    load_chunk = True
+    store_loss = True
+    use_preprocessed = True
+
+    # others
+    num_gpus = 1
+    log_only_rank0 = False
+    use_jax_distributed = False
+    use_xla_flags = False
+    dask_threads = 8
+    use_half_precision = True
+
+class StackedCP0Tester(StackedCP0Emulator):
+    target_lead_time = ["6h", "12h", "18h", "24h", "30h", "36h", "42h", "48h"]
+
+tree_util.register_pytree_node(
+    StackedCP0Emulator,
+    StackedCP0Emulator._tree_flatten,
+    StackedCP0Emulator._tree_unflatten
+)
+
+tree_util.register_pytree_node(
+    StackedCP0Tester,
+    StackedCP0Tester._tree_flatten,
+    StackedCP0Tester._tree_unflatten
+)
---
+++ prototypes/stacked_cp0/train.py
@@ -0,0 +1,202 @@
+import argparse
+import logging
+import os
+import sys
+import shutil
+from functools import partial
+
+import numpy as np
+import pandas as pd
+import optax
+from graphufs.stacked_training import init_model, optimize
+#from graphufs.stacked_parallel_training import optimize
+from graphufs.datasets import Dataset
+from graphufs.batchloader import BatchLoader
+
+from graphufs.log import setup_simple_log
+from graphufs.utils import get_last_input_mapping
+from graphufs.fvstatistics import FVStatisticsComputer
+from graphufs.training import (
+    init_devices,
+)
+import jax
+
+from config import StackedCP0Emulator
+from graphufs.optim import clipped_cosine_adamw
+
+def calc_stats(Emulator, comp="atm"):
+
+    # This is a bit of a hack to enable testing, for real
+    # cases, we want to compute statistics during preprocessing
+    # Note we want to do this before initializing emulator object
+    # since it tries to pull the statistics there.
+    if comp in ["atm", "ice", "land"]:
+        time_skip = 2 # everything is in 3 hour time steps in fv3
+
+    elif comp.lower() == "ocn".lower():
+        time_skip = 1 # time step is 6 hour for the oceans  
+    
+    else:
+        raise ValueError("comp values can only be atm, ocn, ice, or land")
+   
+    open_zarr_kwargs = {
+        "storage_options": {"token": "anon"},
+    }
+
+    to_zarr_kwargs = {
+        "mode": "a",
+    }
+
+    fvstats = FVStatisticsComputer(
+        path_in=Emulator.data_url[comp],
+        path_out=os.path.dirname(Emulator.norm_urls[comp]["mean"]),
+        interfaces=Emulator.interfaces[comp],
+        comp=comp,
+        start_date=None,
+        end_date=Emulator.training_dates[-1],
+        time_skip=time_skip,
+        load_full_dataset=True,
+        transforms=Emulator.input_transforms,
+        open_zarr_kwargs=open_zarr_kwargs,
+        to_zarr_kwargs=to_zarr_kwargs
+    )
+
+    tisr_args = {}
+
+    if comp.lower() == "atm":
+        all_variables = list(set(
+            Emulator.atm_input_variables + Emulator.atm_forcing_variables + Emulator.atm_target_variables
+        )) 
+        all_variables.append("log_spfh")
+        all_variables.append("log_spfh2m")
+        tisr_args["integration_period"] = pd.Timedelta(hours=6)
+    
+    elif comp.lower() == "ocn":
+        all_variables = list(set(
+            Emulator.ocn_input_variables + Emulator.ocn_forcing_variables + Emulator.ocn_target_variables
+        ))
+    
+    elif comp.lower() == "land": 
+        all_variables = list(set(
+            Emulator.land_input_variables + Emulator.land_forcing_variables + Emulator.land_target_variables
+        ))
+    
+    elif comp.lower() == "ice": 
+        all_variables = list(set(
+            Emulator.ice_input_variables + Emulator.ice_forcing_variables + Emulator.ice_target_variables
+        ))
+    else: 
+        raise ValueError(f"{comp} not supported")
+
+    fvstats(all_variables, **tisr_args)
+
+def train(Emulator):
+
+    # We don't parse arguments since we can't be inconsistent with stats
+    # computed above
+    gufs = Emulator()
+
+    # for multi-gpu training
+    init_devices(gufs)
+
+    # data generators
+    training_data = Dataset(gufs, mode="training")
+    validation_data = Dataset(gufs, mode="validation")
+    # this loads the data in ... suboptimal I know
+    logging.info("Loading Training and Validation Datasets")
+    training_data.xds.load(); 
+    validation_data.xds.load();  
+    logging.info("... done loading")
+
+    trainer = BatchLoader(
+        training_data,
+        batch_size=gufs.batch_size,
+        shuffle=True,
+        drop_last=True,
+        num_workers=gufs.num_workers,
+    )
+    validator = BatchLoader(
+        validation_data,
+        batch_size=gufs.batch_size,
+        shuffle=True,
+        drop_last=True,
+        num_workers=gufs.num_workers,
+    )
+
+    # compute loss function weights once
+    weights = gufs.calc_loss_weights(training_data)
+
+    # this is tricky, because it needs to be "rebuildable" in JAX's eyes
+    # so better to just explicitly pass it around
+    last_input_channel_mapping = get_last_input_mapping(training_data)
+
+    # load weights or initialize a random model
+    logging.info("Initializing Optimizer and Parameters")
+    inputs, _ = trainer.get_data()
+    params, state = init_model(gufs, inputs, last_input_channel_mapping)
+    gufs.save_checkpoint(params, id=0)
+
+    loss_name = f"{gufs.local_store_path}/loss.nc"
+    if os.path.exists(loss_name):
+        os.remove(loss_name)
+
+    # setup optimizer
+    steps_in_epoch = len(trainer)
+    n_total = gufs.num_epochs * steps_in_epoch
+    n_linear = max(10, int(len(trainer)/100))
+    n_cosine = n_total - n_linear
+    optimizer = clipped_cosine_adamw(
+        n_linear=n_linear,
+        n_total=n_total,
+        peak_value=1e-3,
+    )
+
+    logging.info(f"Starting Training with:")
+    logging.info(f"\t batch_size = {gufs.batch_size}")
+    logging.info(f"\t {len(trainer)} training steps per epoch")
+    logging.info(f"\t {len(validator)} validation steps per epoch")
+    logging.info(f"\t ---")
+    logging.info(f"\t {n_linear} linearly increasing LR steps")
+    logging.info(f"\t {n_cosine} cosine decay LR steps")
+    logging.info(f"\t {n_total} total training steps")
+
+    # training
+    opt_state = None
+    for e in range(gufs.num_epochs):
+        logging.info(f"Starting epoch {e}")
+
+        # optimize
+        params, loss, opt_state = optimize(
+            params=params,
+            state=state,
+            optimizer=optimizer,
+            emulator=gufs,
+            trainer=trainer,
+            validator=validator,
+            weights=weights,
+            last_input_channel_mapping=last_input_channel_mapping,
+            opt_state=opt_state,
+        )
+
+        logging.info(f"Done with epoch {e}")
+
+        # save weights
+        gufs.save_checkpoint(params, id=e+1)
+
+    trainer.shutdown(cancel=True)
+    validator.shutdown(cancel=True)
+
+if __name__ == "__main__":
+
+    # logging isn't working for me on PSL, no idea why
+    setup_simple_log()
+
+    # there is a central local stats path for all model components
+    stats_path = os.path.dirname(StackedCP0Emulator.norm_url["mean"])
+
+    if not os.path.isdir(stats_path):
+        logging.info(f"Could not find {stats_path}, computing statistics...")
+        for comp in ["atm", "ocn", "ice", "land"]:
+            calc_stats(StackedCP0Emulator, comp=comp)
+
+    train(StackedCP0Emulator)
