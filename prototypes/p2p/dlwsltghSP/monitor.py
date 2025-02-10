48e02c8c625698dddd6c7c60198af65ed132b1b1
Timothy Andrew Smith <smith.timothy.andrew@gmail.com>
2025-02-10 10:09:59 -0800

    training done

--- prototypes/p2p/dlwsltghSP/monitor.py
+++ prototypes/p2p/dlwsltghSP/monitor.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
 
-    ds = xr.load_dataset("/pscratch/sd/t/timothys/p2p/dlwsltghSP/loss.nc")
+    ds = xr.load_dataset("/global/cfs/cdirs/m4718/timothys/p2p/dlwsltghSP/loss.nc")
 
     fig, axs = plt.subplots(1,2, figsize=(8,4), constrained_layout=True)
 
