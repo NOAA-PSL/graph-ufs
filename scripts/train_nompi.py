4b7ac59a505e28bd47e97429964d01d04bf4c9d5
Niraj Agarwal <niraj.agarwal@noaa.gov>
2025-03-14 14:19:40 -0700

    centrally located training scripts, both mpi and non-mpi

---
+++ scripts/train_nompi.py
@@ -0,0 +1,108 @@
+import os
+import sys
+import logging
+from functools import partial
+
+import dask
+import numpy as np
+import optax
+from graphufs.stacked_training import (
+    optimize,
+    init_model,
+)
+from graphufs.datasets import Dataset, PackedDataset
+from graphufs.tensorstore import PackedDataset as TSPackedDataset, BatchLoader as TSBatchLoader
+from graphufs.batchloader import BatchLoader
+from graphufs.log import setup_simple_log
+
+from graphufs.utils import get_last_input_mapping
+from graphufs import init_devices
+
+def train(RawEmulator, TransformedEmulator):
+
+    # initial setup
+    init_devices(TransformedEmulator)
+
+    # data generators
+    tds = Dataset(RawEmulator, mode="training")
+    training_data = TSPackedDataset(TransformedEmulator, mode="training")
+    validation_data = TSPackedDataset(TransformedEmulator, mode="validation")
+
+
+    trainer = TSBatchLoader(
+        training_data,
+        batch_size=TransformedEmulator.batch_size,
+        shuffle=True,
+        drop_last=True,
+        num_workers=TransformedEmulator.num_workers,
+        max_queue_size=TransformedEmulator.max_queue_size,
+    )
+    validator = TSBatchLoader(
+        validation_data,
+        batch_size=TransformedEmulator.batch_size,
+        shuffle=True,
+        drop_last=True,
+        num_workers=TransformedEmulator.num_workers,
+        max_queue_size=TransformedEmulator.max_queue_size,
+    )
+
+    logging.info("Initializing Loss Function Weights and Stacked Mappings")
+    # compute loss function weights once
+    loss_weights = RawEmulator.calc_loss_weights(tds)
+    last_input_channel_mapping = get_last_input_mapping(tds)
+
+    # initialize a random model
+    logging.info("Initializing Optimizer and Parameters")
+    inputs, _ = trainer.get_data()
+    params, state = init_model(TransformedEmulator, inputs, last_input_channel_mapping)
+    TransformedEmulator.save_checkpoint(params, id=0)
+
+    loss_name = f"{TransformedEmulator.local_store_path}/loss.nc"
+    if os.path.exists(loss_name):
+        os.remove(loss_name)
+
+    # setup optimizer
+    steps_in_epoch = len(trainer)
+    n_total = TransformedEmulator.num_epochs * steps_in_epoch
+    n_linear = 1000
+    n_cosine = n_total - n_linear
+    optimizer = graphufs_optimizer(
+        n_linear=n_linear,
+        n_total=n_total,
+        peak_value=1e-3,
+    )
+
+    logging.info(f"Starting Training with:")
+    logging.info(f"\t batch_size = {TransformedEmulator.batch_size}")
+    logging.info(f"\t {len(trainer)} training steps per epoch")
+    logging.info(f"\t {len(validator)} validation steps per epoch")
+    logging.info(f"\t ---")
+    logging.info(f"\t {n_linear} linearly increasing LR steps")
+    logging.info(f"\t {n_cosine} cosine decay LR steps")
+    logging.info(f"\t {n_total} total training steps")
+
+    # training
+    opt_state = None
+    for e in range(TransformedEmulator.num_epochs):
+        logging.info(f"Starting epoch {e}")
+
+        # optimize
+        params, loss, opt_state = optimize(
+            params=params,
+            state=state,
+            optimizer=optimizer,
+            emulator=TransformedEmulator,
+            trainer=trainer,
+            validator=validator,
+            weights=loss_weights,
+            last_input_channel_mapping=last_input_channel_mapping,
+            opt_state=opt_state,
+        )
+
+        # save weights
+        logging.info(f"Done with epoch {e}")
+        TransformedEmulator.save_checkpoint(params, id=e+1)
+
+    logging.info("Done Training")
+    trainer.shutdown(cancel=True)
+    validator.shutdown(cancel=True)
